1eba8d35533ce0106d4f595459da2ec662d1236b
Luis Gomez <luiskaco@gmail.com>
2021-07-16 01:24:42 -0500

    Agregando servidor apollo client. Configurando apolloClient

---
+++ config/apollo.js
@@ -0,0 +1,25 @@
+// Importamos apollo cliente
+import {ApolloClient} from '@apollo/client';
+
+// nota: Estamos conectando al apollo server
+
+// Importamos la dependencia de cache
+import {InMemoryCache} from 'apollo-cache-inmemory';
+
+// Importamos la dependencia de conexion
+import {HttpLink} from 'apollo-link-http';
+
+
+// objeto cliente apollo
+
+const client = new ApolloClient({
+     cache: new InMemoryCache(), // Manejo de cache
+     link: new HttpLink({
+        uni: 'http://localhost:4000/'
+     })
+
+});
+
+
+
+export default client;
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 /**
  * @format
  */
+import React from 'react';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// Importando apollo
+import client from './config/apollo';
+import { ApolloProvider } from '@apollo/client';
+
+// Nota: Hacemos que este disponible el apollo en toda la aplicacion
+const uptaskApp = () => (
+    <ApolloProvider client={client}>
+         <App />
+    </ApolloProvider>
+)
+
+ /* Agregamos el componente app */
+
+
+// AppRegistry.registerComponent(appName, () => App);
+
+// Pasamoes la aplicacion actual mas el provider
+AppRegistry.registerComponent(appName, () => uptaskApp);
